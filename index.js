556f26072b4bc301879d9794f41d243bab112aa4
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-08 14:12:09 +0300

    fix: phonebook database step 7

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: "Malformatted id" });
   }
 
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
   next(error);
 };
 
--- model/person.js
+++ model/person.js
@@ -15,7 +15,12 @@ moongose
   });
 
 const personSchema = new moongose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: [3, "Name must be at least 3 characters long"],
+    unique: true,
+  },
   number: String,
 });
 
